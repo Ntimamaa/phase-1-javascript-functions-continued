574720918e9572cb929014e7608d8c7a88062aae
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-12 17:35:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun(); 
+saturdayFun('partying'); 
+
+
+const mondayWork = function(work = 'go to the office') {
+  return `This Monday, I will ${work}.`;
+}
+
+mondayWork();
+mondayWork('work from home'); 
+
+
+function wrapAdjective(wrap) {
+    if(wrap === '*'){
+  return function(adjective = 'a hard worker') {
+    return `You are ${wrap}${adjective}${wrap}!`;
+  }
+    }
+    else if(wrap === '||'){
+        return function(adjective = 'a dedicated programmer') {
+            return `You are ${wrap}${adjective}${wrap}!`;
+          }
+    }
+}
+wrapAdjective('*');
+wrapAdjective('||')
+
